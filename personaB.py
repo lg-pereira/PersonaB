d694fe4f67666e03becc990dfef7903defd32670
Luiz Gustavo Pereira <lgonlineworker@gmail.com>
2025-02-11 14:04:37 -0300

    Update personaB.py

--- personaB.py
+++ personaB.py
@@ -6,7 +6,12 @@ from streamlit_gsheets import GSheetsConnection
 st.title("Read Google Sheet as DataFrame")
 
 conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read(worksheet="Home")
+df = conn.read(
+    worksheet="Home",
+    ttl="10m",
+    usecols=[0, 5],
+    nrows=1,
+)
 
 st.dataframe(df)
   
