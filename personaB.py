520167eb8c615c636c45c7a173960ee9f8342d34
Luiz Gustavo Pereira <lgonlineworker@gmail.com>
2025-02-11 13:58:47 -0300

    Update personaB.py

--- personaB.py
+++ personaB.py
@@ -3,16 +3,10 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-# Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
+st.title("Read Google Sheet as DataFrame")
 
-df = conn.read(
-    worksheet="Data",
-    ttl="10m",
-    usecols=[0, 5],
-    nrows=5,
-)
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+conn = st.connection("gsheets", type=GSheetsConnection)
+df = conn.read(worksheet="Example 1")
+
+st.dataframe(df)
   
