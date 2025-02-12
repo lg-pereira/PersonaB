53d1b4b3f746322d940b3a1b1f701dd3f5f08e35
Luiz Gustavo Pereira <lgonlineworker@gmail.com>
2025-02-12 10:24:24 -0300

    Update personaB.py

--- personaB.py
+++ personaB.py
@@ -44,12 +44,12 @@ def start_timer(tempo_segundos, start_sound, end_sound):
             st.warning("Não foi possível tocar a buzina de início")
 
     
-    timer_placeholder = st.subheader() #placeholder para o timer regressivo do jogo
+    timer_placeholder = st.empty() #placeholder para o timer regressivo do jogo
 
     for i in range(tempo_segundos, -1, -1): #contagem do tempo do jogo
         minutos_restantes = i // 60
         segundos_restantes = i % 60
-        countdown_placeholder.write(f"TIMER: {minutos_restantes:02d}:{segundos_restantes:02d}")
+        countdown_placeholder.write(f"###TIMER: {minutos_restantes:02d}:{segundos_restantes:02d}")
         time.sleep(1)
         
     if end_sound:
